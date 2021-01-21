47bbcf5d2d509e7370863b41e4f480691aac0c24
Vladimir Ermukhanov <ermuhanoff@gmail.com>
2021-01-21 23:31:14 +0400

    fix: some fixes

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
-const express = require('express');
+const express = require("express");
+const bodyParser = require("body-parser");
 
 const app = express();
+const urlencodedParser = bodyParser.urlencoded({extended: false});
 
-app.get("/", function(request, response){
-    response.sendFile(__dirname + "/index.html");
+app.use("/", function (request, response) {
+  response.sendFile(__dirname + "/index.html");
 });
-
+app.use("/:bad", urlencodedParser, function (request, response) {
+  response.send("<h1>Page not found!</h1>");
+});
 
 app.listen(process.env.PORT || 3000);
